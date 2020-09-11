57173c51423e73fd312669a9e5cbc028f678c568
Aayush <43297314+Aayush9029@users.noreply.github.com>
2020-09-11 12:56:06 -0400

    added class.py
    only checks if the site is up or not.
    In-depth checking is being worked upon.

---
+++ python_scripts/eclass.py
@@ -0,0 +1,48 @@
+import urllib.request
+from os import system
+from time import sleep
+
+
+sleep_time = 3 # this is check if the site is up every three seconds (incase requests times out)
+
+# ^ failsafe timeout 
+
+
+def site_is_up():
+    try:
+        system("say 'E-class is up'")
+    except:
+        # not sure if this works on windows, hopefully it does
+        system("echo 'E-class is up'|ptts")
+    
+    exit("eclass is up !!")
+
+
+def check_if_site_is_up():
+    status_code = None
+    try:
+        status_code = urllib.request.urlopen("https://eclass.yorku.ca").getcode()
+        print(type(status_code))
+        print(status_code)
+        if status_code == 200:
+            return True
+    except:
+        # who cares about handling errors amiright
+        pass
+    return False
+
+
+def main():
+    while True:
+        if check_if_site_is_up():
+            site_is_up()
+            # break
+        sleep(sleep_time)
+
+
+
+if __name__ == "__main__":
+    print("The program has started, it will notify you once the site is up and running..")
+    main()
+
+
